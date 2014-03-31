a7490692aba724f34279eeb9a0d68bdb97b411ec
Daniel Garzon <dg2796@columbia.edu>
2014-03-31 19:47:41 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CeeDee::Application.config.secret_key_base = '91d7d41785bf96f1429ca267e3dc9941a8479c1c3dd8292677adc676ac9909fa04251e4d1f0cfc274502228acd952d1afe833148e268589f2d4f0de6c2d1b7fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CeeDee::Application.config.session_store :cookie_store, key: '_CeeDee_session'
