ec3d2b05cd39183b49960a96c9b9bef081d198a0
Daniel Garzon <dg2796@columbia.edu>
2014-04-29 18:11:27 -0400

    Demo version.

--- app/models/album.rb
+++ app/models/album.rb
@@ -11,11 +11,16 @@ class Album < ActiveRecord::Base
   def self.create_from_spotify (spotify_id, image_url, user_id, rating_val, comment_val, genre_val)
     response = MetaSpotify::Album.lookup(spotify_id, :extras => 'trackdetail')
 
+    if Album.last.nil?
+      aid = 1
+    else
+      aid = Album.last.id + 1
+    end
     album = Album.create(:spotify_id => spotify_id,
                          :image_url => image_url,
                          :user_id => user_id,
                          :album_name => response.name,
-                         :album_id => Album.last.id + 1)
+                         :album_id => aid)
 
     genre = Genre.where(:genre_name => genre_val, :user_id => user_id).first
 
@@ -31,12 +36,22 @@ class Album < ActiveRecord::Base
 
     album.genre_id = genre.id
 
-    rating = Rating.create(:rating_id => Rating.last.id + 1, :rating_value => rating_val, :album_id => album.id)
+    if Rating.last.nil?
+      rid = 1
+    else
+      rid = Rating.last.id + 1
+    end
+    rating = Rating.create(:rating_id => rid, :rating_value => rating_val, :album_id => album.id)
     rating.save!
 
     album.rating_id = rating.id
 
-    comment = Comment.create(:comment_id => Comment.last.id + 1, :comment_description => comment_val, :album_id => album.id, :user_id => user_id)
+    if Comment.last.nil?
+      cid = 1
+    else
+      cid = Comment.last.id + 1
+    end
+    comment = Comment.create(:comment_id => cid, :comment_description => comment_val, :album_id => album.id, :user_id => user_id)
     comment.save!
 
     album.comment_id = comment.id
@@ -70,7 +85,12 @@ class Album < ActiveRecord::Base
     album.band_id = band.id
 
     response.tracks.each do |track|
-      song = Track.create(:track_id => Track.last.id + 1,
+      if Track.last.nil?
+        tid = 1
+      else
+        tid = Track.last.id + 1
+      end
+      song = Track.create(:track_id => tid,
                           :album_id => album.id,
                           :track_name => track.name,
                           :track_duration => track.length,
